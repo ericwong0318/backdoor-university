11e81110bc93d2f32646d034daffb829256629af
Andrew SZETO <zkwokleung@gmail.com>
2022-03-18 11:21:30 +0800

    Update: App layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import './App.css';
+import Footer from './Layouts/Footer/Footer';
+import Header from './Layouts/Header/Header';
+import Main from './Layouts/Main/Main';
 import SignIn from './signin';
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
