cafb0e1604e70b533e8d44a8fcca054c900ec658
Eric Wong <ericwong0318@gmail.com>
2022-04-08 12:38:17 +0800

    Updated modify password and forget password

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 
 // encryption
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const salt = 10;
 
 // mail
 const nodemailer = require("nodemailer");
@@ -46,7 +46,7 @@ db.once('open', function () {
     console.log("DB is connected");
 })
 
-// schemas and models
+/* schemas and models */
 const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
@@ -126,97 +126,12 @@ const reportSchema = Schema({
 });
 const Report = mongoose.model('Report', reportSchema);
 
-// workers
+/* 
+* supporting functions 
+*/
 
-
-/*
- * routing
- */
-
-
-/* register account */
-app.post('/register', (req, res) => {
-    /* handle photo */
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).json({ err: 'No files are uploaded.' });
-    }
-    /* encrypt password */
-    bcrypt.genSalt(saltRounds, function (err, salt) {
-        bcrypt.hash(req.body.password, salt, function (err, hash) {
-            let photo = req.files.photo;
-            let uploadPath = __dirname + '/photos/' + photo.name;
-            photo.mv(uploadPath, function (err) { // move photo to folder /photos/
-                if (err) {
-                    return res.status(500).json(err);
-                }
-            });
-
-            User.create(
-                {
-                    email: req.body.email,
-                    password: hash,
-                    name: req.body.name,
-                    photo: uploadPath,
-                    currProgramme: {
-                        school: req.body.school,
-                        programme: req.body.programme,
-                        addmissionYear: req.body.addmissionYear,
-                        cgpa: req.body.cgpa
-                    },
-                    exam: {
-                        name: req.body.examname,
-                        result: req.body.result
-                    },
-                    status: 'unverified', // will change to active after passed email verification
-                    offer: req.body.offer /* need checking correctness*/
-                }, (err, user) => {
-                    if (err) {
-                        res.status(409).json(err);
-                        return
-                    }
-                    /* send email verification */
-                    sendEmailVerification(req.body.email).catch(console.error);
-                    return res.json({ msg: "Please check the veriftication email, including spam folder" });
-                });
-        })
-    });
-});
-
-app.post('/login', (req, res) => {
-    let pwd = req.body.password;
-    /* compare email and password in database and input */
-    bcrypt.genSalt(saltRounds, function (err, salt) {
-        bcrypt.hash(pwd, salt, function (err, hash) {
-            User
-                .findOne({ email: req.body.email, }, 'email password')
-                .exec((err, user) => {
-                    if (user === null) { // email not found
-                        res.status(401).json({ err: 'incorrect email' });
-                        return;
-                    } else {
-                        bcrypt.compare(pwd, user.password, (err, result) => {
-                            if (result === true) { // password correct
-                                res.cookie('saltedPassword', hash, { maxAge: 900000 });
-                                res.json({ msg: "Login successful" });
-                                return;
-                            } else {
-                                res.status(401).json({ err: 'incorrect password' });
-                                return;
-                            }
-                        });
-                    }
-                });
-        });
-    });
-});
-
-/* verify email */
-app.get('/send-verfiy-email', (req, res) => {
-    sendEmailVerification(req.body.email).catch(console.error);
-    res.json({ msg: "A verfication email is sent" });
-});
-
-async function sendEmailVerification(email) {
+/* send email for verification or reset password */
+async function sendEmail(email, option, newPassword) {
     let transporter = nodemailer.createTransport({
         host: "smtp.ethereal.email",
         port: 587,
@@ -232,26 +147,123 @@ async function sendEmailVerification(email) {
         }
     });
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: 'Backdoor University',
-        to: email, // list of receivers
-        subject: "Click the Link for register",
-        html: `<h1>Backdoor University</h1>
+
+    let info;
+    switch (option) {
+        case "verify":
+            info = await transporter.sendMail({
+                from: 'Backdoor-University@gmail.com',
+                to: email, // list of receivers
+                subject: "Click the Link for register",
+                html: `<h1>Backdoor University</h1>
         <h3>Please click the following Link for register: </h3>
         <a href="http://localhost:3001/activate-email/${email}">
         http://localhost:3001/activate-email/${email}
         </a>`
-    });
+            });
+            console.log("Message sent: %s", info.messageId);
+            return;
 
-    console.log("Message sent: %s", info.messageId);
-    return;
+        case "reset":
+            info = await transporter.sendMail({
+                from: 'Backdoor-University@gamil.com',
+                to: email, // list of receivers
+                subject: "Your new password",
+                html: `<h1>Backdoor University</h1>
+                        <h3>Your new password: ${newPassword} </h3>`
+            });
+            console.log("Message sent: %s", info.messageId);
+            return;
+        default:
+            console.log("Wrong option for this function");
+    }
 }
 
+/*
+ * routing
+ */
+
+/* register account */
+app.post('/register', (req, res) => {
+    /* handle photo */
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({ err: 'No files are uploaded.' });
+    }
+    /* encrypt password */
+    bcrypt.hash(req.body.password, salt, (err, hash) => {
+        // move photo to folder /photos /
+        let photo = req.files.photo;
+        let uploadPath = __dirname + '/photos/' + photo.name;
+        photo.mv(uploadPath, function (err) {
+            if (err) {
+                return res.status(500).json(err);
+            }
+        });
+
+        // create user
+        User.create(
+            {
+                email: req.body.email,
+                password: hash,
+                name: req.body.name,
+                photo: uploadPath,
+                currProgramme: {
+                    school: req.body.school,
+                    programme: req.body.programme,
+                    addmissionYear: req.body.addmissionYear,
+                    cgpa: req.body.cgpa
+                },
+                exam: {
+                    name: req.body.examname,
+                    result: req.body.result
+                },
+                status: 'unverified', // will change to active after passed email verification
+                offer: req.body.offer /* need checking correctness*/
+            }, (err, user) => {
+                if (err) {
+                    res.status(409).json(err);
+                    return
+                }
+                /* send email verification */
+                sendEmail(req.body.email, "verify").catch(console.error);
+                return res.json({ msg: "Please check the veriftication email, including spam folder" });
+            });
+    })
+
+});
+
+/* login todo: cookie? */
+app.post('/login', (req, res) => {
+    let pwd = req.body.password;
+    /* compare email and password in database and input */
+    User
+        .findOne({ email: req.body.email, status: "active" }, 'email password')
+        .exec((err, user) => {
+            if (user === null) { // email not found
+                res.status(401).json({ err: 'Incorrect email or This email is not verified' });
+                return;
+            } else {
+                bcrypt.compare(pwd, user.password, (err, result) => {
+                    if (result === true) { // password correct
+                        // res.cookie('saltedPassword', hash, { maxAge: 900000 });
+                        res.json({ msg: "Login successful" });
+                        return;
+                    } else {
+                        res.status(401).json({ err: 'Incorrect password' });
+                        return;
+                    }
+                });
+            }
+        });
+
+});
+
+
+
 app.get('/activate-email/:email', (req, res) => {
-    let e = req.params.email;
+    let userEmail = req.params.email;
     /* set account status to active */
-    User.findOne({ email: e }, {}, {}, (err, user) => {
+    User.findOne({ email: userEmail }, {}, {}, (err, user) => {
         if (err) {
             return res.json(err);
         }
@@ -261,6 +273,64 @@ app.get('/activate-email/:email', (req, res) => {
             return res.json({ msg: "Account is activated" });
         }
     });
+});
+
+/* modify password */
+app.post('/modify-password', (req, res) => {
+    let oldPwd = req.body.oldPassword;
+    let newPwd = req.body.newPassword;
+
+    /* compare email and old password in database and input */
+    User
+        .findOne({ email: req.body.email }, 'email password')
+        .exec((err, user) => {
+            if (user === null) { // email not found
+                res.status(401).json({ err: 'Incorrect email' });
+                return;
+            } else {
+                bcrypt.compare(oldPwd, user.password, (err, result) => {
+                    if (result === true) { // password correct
+                        bcrypt.hash(newPwd, salt, function (err, newPwdHash) {
+                            user.password = newPwdHash;
+                            user.save();
+                        })
+
+                        res.json({ msg: "Modify password successful" });
+                        return;
+                    } else {
+                        res.status(401).json({ err: 'Incorrect old password' });
+                        return;
+                    }
+                });
+            }
+        });
+
+});
+
+/* foget password */
+app.post('/forget-password', (req, res) => {
+    let userEmail = req.body.email;
+
+    User
+        .findOne({ email: userEmail }, 'email password')
+        .exec((err, user) => {
+            if (user === null) {
+                return res.status(401).json({ err: "Incorrect email" });
+            }
+            else {
+                // g(userEmail, user, res);
+                let newPwd = Math.floor(Math.random() * 999).toString();
+                console.log(newPwd);
+                bcrypt.hash(newPwd, salt, function (err, newPwdHash) {
+                    console.log(newPwdHash);
+                    user.password = newPwdHash;
+                    user.save();
+                })
+                res.json({ msg: "The system has reset a new password. Please check your email for it" });
+                sendEmail(userEmail, "reset", newPwd);
+                return;
+            };
+        })
 });
 
 const server = app.listen(3001);
