a916fea5e8f2e7b725516d7318f567c8b23f6b61
RickyYeung402 <100333147+RickyYeung402@users.noreply.github.com>
2022-04-11 00:45:31 +0800

    Add translation

--- frontend/src/Localizations/ForgotPasswordLocalizationStrings.tsx
+++ frontend/src/Localizations/ForgotPasswordLocalizationStrings.tsx
@@ -36,4 +36,20 @@ ForgotPasswordLocalizationStrings = new LocalizedStrings(
             server_unavailable_error: "Sorry! The server is current down for maintainance!",
             email_unregistered_error: "This email has not been registered!",
         }
+        /* zh: {
+            forgot_password: "忘記密碼",
+            reset_password: "重置密碼",
+            email: "電郵地址",
+            hint: "請輸入你的電郵地址以重置密碼",
+            wait: "請稍候片刻 . . .",
+            submit: "提交",
+            field_empty_error: "* 必須填寫！",
+            success: "成功",
+            success_message: "請檢查你的電子郵箱以重置密碼！",
+            back_to_login: "返回登入頁",
+            no_account_error: "這個電郵尚未登記！",
+            invalid_email_format_error: "無效電郵地址",
+            server_unavailable_error: "對不起！伺服器暫時停止運作以供維收！",
+            email_unregistered_error: "這個電郵尚未登記！",
+        } */
     })
