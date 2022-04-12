696db80cef6567cf9c451180394033a94683de24
Eric Wong <ericwong0318@gmail.com>
2022-04-13 00:56:22 +0800

    Fix LoginController.js

--- backend/controllers/LoginController.js
+++ backend/controllers/LoginController.js
@@ -19,7 +19,24 @@ exports.userOrAdminUpdatePassword = (req, res) => {
             'email password')
         .exec((err, user) => {
             if (user === null) { // email not found
-                return;
+
+                /* if the email is a admin email */
+                Admin
+                    .findOne({ email: req.body.email }, 'email password')
+                    .exec((err, admin) => {
+                        if (admin === null) { // email not found
+                            res.status(401).json({ err: 'Incorrect email for both user and admin roles' });
+                            return;
+                        } else {
+                            bcrypt.hash(newPwd, salt, function (err, hash) {
+                                admin.password = hash;
+                                admin.save();
+                                res.json({ msg: "Modify admin password successful" });
+                                return;
+                            });
+                        };
+                    });
+                    
             } else {
                 bcrypt.hash(newPwd, salt, function (err, hash) {
                     user.password = hash;
@@ -30,20 +47,5 @@ exports.userOrAdminUpdatePassword = (req, res) => {
             };
         });
 
-    /* if the email is a admin email */
-    Admin
-        .findOne({ email: req.body.email }, 'email password')
-        .exec((err, admin) => {
-            if (admin === null) { // email not found
-                res.status(401).json({ err: 'Incorrect email for both user and admin roles' });
-                return;
-            } else {
-                bcrypt.hash(newPwd, salt, function (err, hash) {
-                    admin.password = hash;
-                    admin.save();
-                    res.json({ msg: "Modify admin password successful" });
-                    return;
-                });
-            };
-        });
+
 }
