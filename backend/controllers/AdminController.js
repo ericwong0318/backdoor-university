ad4c26bfc985ca9b80f198224d6bc72db2d86459
Eric Wong <ericwong0318@gmail.com>
2022-04-13 00:09:48 +0800

    Update error handling

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -8,11 +8,13 @@ exports.adminLogin = (req, res) => {
     Admin
         .findOne({ email: req.body.email }, 'email password')
         .exec((err, admin) => {
+            if (err) return res.json({ err: err });
             if (admin === null) { // email not found
                 res.status(401).json({ err: "Incorrect email" });
                 return;
             } else {
                 bcrypt.compare(req.body.password, admin.password, (err, result) => {
+                    if (err) return res.json({ err: err });
                     if (result === true) { // password correct
                         // res.cookie('saltedPassword', hash, { maxAge: 900000 });
                         res.json({ msg: "Login successful" });
