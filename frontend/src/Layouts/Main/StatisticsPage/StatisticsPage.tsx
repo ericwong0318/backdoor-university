e6c9710c2c61f8e07f7e4ca9dd63eea9fc3ac7a7
Eric Wong <ericwong0318@gmail.com>
2022-04-13 13:57:34 +0800

    Add comment functions

--- frontend/src/Layouts/Main/StatisticsPage/StatisticsPage.tsx
+++ frontend/src/Layouts/Main/StatisticsPage/StatisticsPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import './StatisticsPage.css'
 
@@ -7,6 +8,7 @@ import { LanguageContext } from '../../../Components/LanguageProvider/LanguagePr
 
 
 import AllStat from "./AllStat";
+import { data1 } from './SpecDataSet';
 const GenStat = () => <AllStat />;
 
 interface IStatisticsPageProps {
@@ -14,6 +16,7 @@ interface IStatisticsPageProps {
 }
 
 const StatisticsPage = (props: IStatisticsPageProps) => {
+
     const { localString } = useContext(LanguageContext)
     return (
         <React.Fragment>
