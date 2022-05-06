d8863e4e37d17caffd77549bb217b3356aaf8883
Andrew SZETO <zkwokleung@gmail.com>
2022-05-06 17:03:53 +0800

    Add auth component documentations

--- frontend/src/Components/auth/RequireAuth.tsx
+++ frontend/src/Components/auth/RequireAuth.tsx
@@ -1,3 +1,14 @@
+/*
+    This file contained the definition of the <RequireAuth/> component.
+    Childs of this component are only accessible after the user has logged in.
+    Otherwise, they will be directed to the login page if they are trying to access the childs without logging in.
+    This component should be used along with react-router for better handling of the routing.
+
+    Example:
+        - To create a login-required route:
+            <Route path={examplePath} element={<RequireAuth><ExampleComponent /></RequireAuth>} />
+*/
+
 import React, { useEffect } from "react";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { LayoutPath } from '../../App/constants';
