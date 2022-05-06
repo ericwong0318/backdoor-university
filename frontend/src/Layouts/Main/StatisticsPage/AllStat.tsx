3a1d11c7788b4b58bdfa175e214160e6c236d4c5
Eric Wong <ericwong0318@gmail.com>
2022-05-06 14:27:08 +0800

    fix: comment

--- frontend/src/Layouts/Main/StatisticsPage/AllStat.tsx
+++ frontend/src/Layouts/Main/StatisticsPage/AllStat.tsx
@@ -33,6 +33,9 @@ import { getAllProgramme } from '../../../features/services';
 
 import { useAuth } from '../../../Components/auth/AuthProvider';
 
+let uniprog = "Computer Science";
+let ccprog = "Information Technology";
+
 // Use Interface to define what this component can take in
 interface ITemplateComponentProps {
 }
@@ -65,7 +68,7 @@ const TemplateComponent = (props: ITemplateComponentProps) => {
             alert("Please login");
         } else {
             // data1.unshift({ id: "1", College: "HKCC", gpa: 3.5, uni: "CUHK", comment: inComment, uniprog: "Computer Science", ccprog: "Information Technology" },);
-            data1.unshift({ id: "1", College: currUser.school, gpa: currUser.cgpa, uni: "CUHK", comment: inComment, uniprog: "Computer Science", ccprog: "Information Technology" },);
+            data1.unshift({ id: "1", College: currUser.school, gpa: currUser.cgpa, uni: "CUHK", comment: inComment, uniprog: uniprog, ccprog: ccprog },);
             console.log(data1);
             setRow2([createData2(data1[0].College, data1[0].gpa, data1[0].uni, data1[0].comment, data1[0].uniprog, data1[0].ccprog)]);
         }
